4eda01908845b17d9a1fe37391ce7808ae17463c
n1rut <nikak14315@gmail.com>
2023-06-01 21:20:04 +0700

    Исправлена некорректная работа

--- Decrypt  n encr/Encryption.cpp
+++ Decrypt  n encr/Encryption.cpp
@@ -101,26 +101,75 @@ void ShamirCode() {
 //Encryption with Hill cipher
 void HillCode(int codeCheck)
 {
-	vector<char> alf;
+	vector<char> alf;// Латинские буквы и символы
 	initializeAlphabet(alf);
 
 	vector<int> codByAlfKey;
 	vector<vector<int>> matrixKey;
+	int g = 0;
 	string someKey = "";
 	int obrEl = 100;
-
 	if (codeCheck != 1)
 	{
-		codByAlfKey = generateKey(alf);
-		matrixKey = generateMatrixKey(codByAlfKey);
-
+		while (1)
+		{
+			codByAlfKey.clear();
+			matrixKey.clear();
+			someKey = "";
+			for (int i = 0; i < 9; i++)
+			{
+				someKey += alf[rand() % alf.size()];
+			}
+			for (char i : someKey)
+			{
+				for (int j = 0; j < alf.size(); j++)
+				{
+					if (i == alf[j])
+					{
+						codByAlfKey.push_back(j);
+					}
+				}
+			}
+			for (int i = 0; i < (int)rint(sqrt(someKey.length())); i++)
+			{
+				vector<int> m;
+				matrixKey.push_back(m);
+			}
+			g = 0;
+			for (int i = 0; i < (int)rint(sqrt(someKey.length())); i++)
+			{
+				for (int j = 0; j < (int)rint(sqrt(someKey.length())); j++)
+				{
+					matrixKey[i].push_back(codByAlfKey[g++]);
+				}
+			}
+			int detMatrixKey = determCalk(matrixKey, matrixKey.size());;
+			int x = rasAlgEvkl(detMatrixKey, alf.size());
+			int obrEl = 0;
+			if ((detMatrixKey < 0 && x > 0) || (detMatrixKey > 0 && x > 0))
+			{
+				obrEl = x;
+			}
+			if (detMatrixKey < 0 && x < 0)
+			{
+				obrEl = -x;
+			}
+			if (detMatrixKey > 0 && x < 0)
+			{
+				obrEl = alf.size() + x;
+			}
+			if (obrEl < 97)
+			{
+				break;
+			}
+		}
 		ofstream fin1("keys.txt");
 		fin1 << someKey << endl;
 		fin1.close();
 	}
 
-	ifstream fout("Some_text.txt");
-	ofstream fin("Str_aft_proc.txt");
+	ifstream fout("Some_text.txt");//читает файл с нашим текстом
+	ofstream fin("Str_aft_proc.txt"); //очищает файл Str_aft_proc.txt куда записывает зашифрованный текст
 
 	string key = "";
 	ifstream fout1("keys.txt");
@@ -129,9 +178,31 @@ void HillCode(int codeCheck)
 
 	if (codeCheck == 1)
 	{
-		codByAlfKey = generateKey(alf);
-		matrixKey = generateMatrixKey(codByAlfKey);
+		for (char i : key)
+		{
+			for (int j = 1; j < alf.size(); j++)
+			{
+				if (i == alf[j])
+				{
+					codByAlfKey.push_back(j);
+				}
+			}
+		}
+		for (int i = 0; i < (int)rint(sqrt(key.length())); i++)
+		{
+			vector<int> m;
+			matrixKey.push_back(m);
+		}
+		g = 0;
+		for (int i = 0; i < (int)rint(sqrt(key.length())); i++)
+		{
+			for (int j = 0; j < (int)rint(sqrt(key.length())); j++)
+			{
+				matrixKey[i].push_back(codByAlfKey[g++]);
+			}
+		}
 	}
+
 
 	system("CLS");
 	funkPrinciple(1);
@@ -143,9 +214,75 @@ void HillCode(int codeCheck)
 	{
 		string inputString = "";
 		getline(fout, inputString);
-
-		vector<vector<int>> encryptStr = encryptString(inputString, matrixKey, alf);
-
+		vector<int> codByAlfStr;
+		for (char i : inputString)
+		{
+			for (int j = 0; j < alf.size(); j++)
+			{
+				if (i == alf[j])
+				{
+					codByAlfStr.push_back(j);
+				}
+			}
+		}
+		vector<vector<int>> matrixStr;
+		int crat = 0;
+		if ((codByAlfStr.size() % (int)rint(sqrt(key.length()))) != 0)
+		{
+			crat = 1;
+		}
+		for (int i = 0; i < codByAlfStr.size() / (int)rint(sqrt(key.length())) + crat; i++)
+		{
+			vector<int> m;
+			matrixStr.push_back(m);
+		}
+		g = 0;
+		for (int i = 0; i < codByAlfStr.size() / (int)rint(sqrt(key.length())) + crat; i++)
+		{
+			if (g < codByAlfStr.size())
+			{
+				for (int j = 0; j < (int)rint(sqrt(key.length())); j++)
+				{
+					matrixStr[i].push_back(codByAlfStr[g++]);
+					if (g >= codByAlfStr.size())
+					{
+						while (matrixStr[i].size() < (int)rint(sqrt(key.length())))
+						{
+							matrixStr[i].push_back(0); ///////////////////////////////////////////////////
+						}
+						break;
+					}
+				}
+			}
+			else
+			{
+				while (matrixStr[i].size() < (int)rint(sqrt(key.length())))
+				{
+					matrixStr[i].push_back(0); ////////////////////////////////////////////////
+				}
+			}
+		}
+		vector<vector<int>> encryptStr;
+		for (int i = 0; i < matrixStr.size(); i++)
+		{
+			vector<int> m;
+			encryptStr.push_back(m);
+		}
+		int mnog = 0;
+		g = -1;
+		for (vector<int> blok : matrixStr)
+		{
+			g++;
+			for (int i = 0; i < blok.size(); i++)
+			{
+				mnog = 0;
+				for (int j = 0; j < blok.size(); j++)
+				{
+					mnog += blok[j] * matrixKey[j][i];
+				}
+				encryptStr[g].push_back(mnog);
+			}
+		}
 		string outputString = "";
 		for (vector<int> blok : encryptStr)
 		{
@@ -154,18 +291,16 @@ void HillCode(int codeCheck)
 				outputString += alf[i % alf.size()];
 			}
 		}
-
 		fin << outputString;
 		cout << outputString << endl;
-
 		if (!fout.eof())
 		{
 			fin << endl;
 		}
 	}
-
 	fout.close();
 	fin.close();
+	cout << endl;
 	system("PAUSE");
 	system("CLS");
 }
