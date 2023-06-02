380703c55caabf31286297af8cc1341aa6d7d0e2
n1rut <nikak14315@gmail.com>
2023-06-02 22:17:34 +0700

    fix

--- Decrypt  n encr/HillComp.h
+++ Decrypt  n encr/HillComp.h
@@ -8,6 +8,4 @@ int rasAlgEvkl(int, int);
 void minorCreat(vector<vector<int>>&, vector<vector<int>>&, int, int);
 int determCalk(vector<vector<int>>, int);
 void initializeAlphabet(vector<char>& alf);
-vector<int> generateKey(const vector<char>& alf);
-vector<vector<int>> generateMatrixKey(const vector<int>& codByAlfKey);
-vector<vector<int>> encryptString(const string& inputString, const vector<vector<int>>& matrixKey, const vector<char>& alf);
+
--- Decrypt  n encr/helpFunc.h
+++ Decrypt  n encr/helpFunc.h
@@ -7,4 +7,3 @@ using namespace std;
 bool checkIfNotANumber(string);							//The function of checking whether the entered string is a number
 uint64_t stepen(uint64_t, int);							//8 byte unsigned int exponentiation function
 uint64_t greatestCommonDivisor(uint64_t, uint64_t);		//Greatest common divisor search function
-#pragma once
